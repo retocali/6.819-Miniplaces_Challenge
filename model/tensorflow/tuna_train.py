a3bee3f03f8b1edfaf24354e2aad41fe3df54b3a
Ubuntu <ubuntu@ip-172-31-89-178.ec2.internal>
2017-11-16 14:11:15 +0000

    Grabbed new results.

--- model/tensorflow/tuna_train.py
+++ model/tensorflow/tuna_train.py
@@ -12,16 +12,25 @@ constant_dropout = alexnet_bn_train.dropout
 constant_learning_rate = alexnet_bn_train.learning_rate
 constant_batch_size = alexnet_bn_train.batch_size
 
+print("-"*16)
+print("\nTESTING DROPOUT\n");
+print("-"*16)
 for dropout in arange(dropout_range[0], dropout_range[1], dropout_range[2]):
 	for _ in range(noise_repeating_rate):
 		alexnet_bn_train.alex_net_run(dropout, constant_batch_size, constant_learning_rate, training_iters)
 
 
+print("-"*19)
+print("\nTESTING BATCH_SIZE\n");
+print("-"*19)
 for batch_size in arange(batch_size_range[0], batch_size_range[1], batch_size_range[2]):
 	for _ in range(noise_repeating_rate):
 		alexnet_bn_train.alex_net_run(constant_dropout, batch_size, constant_learning_rate, training_iters)
 
 
+print("-"*25)
+print("\nTESTING LEARNING_RATE\n");
+print("-"*25)
 for learning_rate in arange(learning_rate_range[0], learning_rate_range[1], learning_rate_range[2]):
 	for _ in range(noise_repeating_rate):
 		alexnet_bn_train.alex_net_run(constant_dropout, constant_batch_size, learning_rate, training_iters)
