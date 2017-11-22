52463d4068fe57eed15ec107e0169d38b28ea218
Davis Tran <dvtran@mit.edu>
2017-11-21 19:21:23 -0500

    final pretty plots!

--- model/tensorflow/plotter.py
+++ model/tensorflow/plotter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Import Data
 resnetdirectory = './resnet/plots/'
 resnetlossfile = 'resnet_losses.npy'
 resnetaccuracyfile = 'resnet_accuracies.npy'
@@ -14,9 +15,15 @@ resnet_accuracies = 100*np.genfromtxt(resnetdirectory + resnetaccuracyfile)
 alexnet_losses = np.genfromtxt(alexnetdirectory + alexnetlossfile)
 alexnet_accuracies = 100*np.genfromtxt(alexnetdirectory + alexnetaccuracyfile)
 
+# Manipulate Data
+data_divide = 5
+n = '-'
+iterations = np.arange(0,10000,data_divide)
+
+# Plot
 plt.figure(1)
-plt.plot(resnet_losses, 'b-', label='AlexNet Losses')
-plt.plot(alexnet_losses, 'r-', label='ResNet Losses')
+plt.plot(iterations, resnet_losses[::data_divide], 'b'+n, alpha=0.7, label='ResNet Losses')
+plt.plot(iterations, alexnet_losses[::data_divide], 'r'+n, alpha=0.7, label='AlexNet Losses')
 plt.title('AlexNet vs. ResNet Losses')
 plt.axis([-10, 10000, 0, 6])
 plt.legend(loc='upper right')
@@ -26,8 +33,8 @@ plt.savefig('./plots/losscomparison.png', bbox_inches='tight')
 
 
 plt.figure(2)
-plt.plot(resnet_accuracies, 'b-', label='AlexNet Top 5 Accuracies')
-plt.plot(alexnet_accuracies, 'r-', label='ResNet Top 5 Accuracies')
+plt.plot(iterations, resnet_accuracies[::data_divide], 'b'+n, alpha=0.7, label='ResNet Top 5 Accuracies')
+plt.plot(iterations, alexnet_accuracies[::data_divide], 'r'+n, alpha=0.7, label='AlexNet Top 5 Accuracies')
 plt.title('AlexNet vs. ResNet Accuracies')
 plt.legend(loc='lower right')
 plt.ylabel('Accuracy [%]')
